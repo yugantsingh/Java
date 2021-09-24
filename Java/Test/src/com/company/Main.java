953985b45355d1afc50257486d726c935aac703b
Yugant Singh <yugantx@gmail.com>
2021-09-25 01:56:03 +0530

    Grocery list item Application v1.00

--- Java/Test/src/com/company/Main.java
+++ Java/Test/src/com/company/Main.java
@@ -1,10 +1,67 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
+
+    private static com.company.groceryList groceryList =  new groceryList();
 
     public static void main(String[] args) {
-	Car tata = new Car();
-    tata.setWheels(4);
-    System.out.println(tata.getWheels());
+        boolean quit = false;
+        int choice;
+        printInstructions();
+        while (!quit){
+            System.out.println("Enter your choice");
+            choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 0 -> printInstructions();
+                case 1 -> printList();
+                case 2 -> addItem();
+                case 3 -> modifyItem();
+                case 4 -> removeItem();
+                case 5 -> searchItem();
+                case 6 -> quit = true;
+            }
+        }
+
+    }
+    public static void printInstructions(){
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for am item in the list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+    public static void addItem(){
+        System.out.print("Please Enter the grocery item :");
+        groceryList.addGroceryItem(sc.nextLine());
+    }
+    public static void modifyItem(){
+        System.out.print("Enter the Item number: ");
+        int position = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter replacement Item: ");
+        groceryList.modifyGroceryItem(position-1, sc.nextLine());
+    }
+    public static void printList(){
+        groceryList.printGroceryList();
+    }
+    public static void removeItem(){
+        System.out.print("Enter the item number to be removed: ");
+        groceryList.removeItem(sc.nextInt());
+        sc.nextLine();
+    }
+    public static void searchItem(){
+        System.out.print("Enter item to be searched: ");
+        if (groceryList.findItem(sc.nextLine())!= null){
+            System.out.println("Item Found");
+        }
+        else
+            System.out.println("Item not found");
     }
 }
---
+++ Java/Test/src/com/company/groceryList.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class groceryList {
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+    }
+    public void printGroceryList(){
+        System.out.println("We have "+ groceryList.size() +" items in list");
+        for (int i=0;i<groceryList.size();i++){
+            System.out.println((i+1)+". "+groceryList.get(i));
+        }
+    }
+    public void modifyGroceryItem(int position,String newItem){
+        groceryList.set(position, newItem);
+    }
+    public void removeItem(int position){
+        groceryList.remove(position-1);
+    }
+    public String findItem(String searchItem){
+//        boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        if (position >= 0){
+            return groceryList.get(position);
+        }
+        return null;
+//        return searchItem;
+    }
+
+
+//    public void main(String[] args) {
+//        String item;
+//        System.out.println("Enter Elements in the grocery list");
+//        for (int i=0; i<5;i++){
+//            item = sc.nextLine();
+//            addGroceryItem(item);
+//        }
+//        printGroceryList();
+//    }
+}
